bfed710033dde8a06642c377bf4cbae43afc61dc
Paweł Korniak <pawello1020@gmail.com>
2021-01-31 23:16:00 +0100

    java11

--- src/main/java/forexApp/comunication/ComunicationChannel.java
+++ src/main/java/forexApp/comunication/ComunicationChannel.java
@@ -32,25 +32,8 @@ public class ComunicationChannel {
     }
 
     private String answerForHelp() {
-        return """
-                 Available commands : "
-                 - get high/low/open/close yyyy.mm.dd hh:mm
-                 - get high/low/open/close yyyy.mm.dd hh
-                 - get high/low/open/close yyyy.mm.dd
-                 - get yyyy.mm.dd hh:mm 
-                 - get yyyy.mm.dd hh
-                 - get yyyy.mm.dd
-                 - volatility yyyy.mm.dd-hh:mm
-                 - volatility yyyy.mm.dd-hh
-                 - volatility yyyy.mm.dd
-                 - most_volatile_day
-                 - most_volatile_hour
-                 - average_minutely_volatility
-                 - average_hourly_volatility
-                 - average_daily_volatility
-                """;
+        return "help";
     }
-
     /**
      * loads file to forexData field
      * @param path
